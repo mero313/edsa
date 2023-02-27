0e3762d5d905f9883677bb179ef0fcd09faae561
mero313 <ameer_mazin2001@yahoo.com>
2023-02-27 22:09:21 +0300

    desk

--- tech/settings.py
+++ tech/settings.py
@@ -127,3 +127,9 @@ STATICFILES_DIRS= [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#media
+
+MEDIA_ROOT =os.path.join(BASE_DIR , 'media')
+MEDIA_URL='/media/'
+
