149c72ae02c31b8cf447208e20a017a4b041f90e
mero313 <ameer_mazin2001@yahoo.com>
2023-02-28 00:59:33 +0300

    desk

---
+++ account/urls.py
@@ -0,0 +1,12 @@
+from . import views
+from django.urls import path , include
+from . import views 
+
+
+urlpatterns = [
+    path('' , views.sign_in , name='sign_in'),
+      path('sign_up/' , views.sign_up , name='sign_up')
+   
+    
+
+]
--- tech/urls.py
+++ tech/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('home.urls')),
-    path('country/' , include('country.urls'))
+    path('country/' , include('country.urls')),
+    path('account/' , include('account.urls'))
 ]+ static(settings.MEDIA_URL ,document_root = settings.MEDIA_ROOT)
 
 
