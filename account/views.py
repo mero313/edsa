78301d8010e003c982634956c4ef816e9bce8d98
mero313 <ameer_mazin2001@yahoo.com>
2023-03-01 23:05:09 +0300

    laptop

--- account/views.py
+++ account/views.py
@@ -4,10 +4,12 @@ from .models import sign_in
 # Create your views here.
 
 
-def sign_in1(request, method = 'POST'):
+def sign_inq(request, method = 'POST'):
     username=request.POST.get('username')
     password=request.POST.get('password')
-    data = sign_in(username=username, password=password)
+    print(username)
+    print(password)
+    data = sign_in(username=username  , password=password)
     data.save()
     
     return render(request, 'sign_in.html')
