042d8895951112e5624f74b2ca4073a1c754d7bc
mero313 <ameer_mazin2001@yahoo.com>
2023-03-01 17:39:49 +0300

    laptop

--- account/views.py
+++ account/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-
+from .models import sign_in
 # Create your views here.
 
 
-
 def sign_in(request):
-    return render (request , 'sign_in.html')
-
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    data = sign_in(username=username, password=password)
+    data.save()
+    return render(request, 'sign_in.html')
 
 
 def sign_up(request):
-    return render (request , 'sign_up.html')
-
+    return render(request, 'sign_up.html')
