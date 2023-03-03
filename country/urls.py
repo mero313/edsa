ab69b8eb9b463742b1503a347a3f7d9bf87679b0
mero313 <ameer_mazin2001@yahoo.com>
2023-03-03 20:20:20 +0300

    desk

--- country/urls.py
+++ country/urls.py
@@ -3,7 +3,7 @@ from django.urls import path , include
 from . import views 
 
 urlpatterns = [
-     path('' , views.country , name='country' ),
+     path('' , views.country1 , name='country1' ),
     path('<int:id>' , views.country_id , name='country_id'),
     
     
--- country/views.py
+++ country/views.py
@@ -4,9 +4,14 @@ from .models import People , Custom , Event , Food
 # Create your views here.
 
 
-def country(request): 
-    x={'people': People.objects.all()}
-    return render(request , 'country.html' , x)   
+
+def country1(request): 
+    return render(request , 'country1.html' )   
+
+
+    #def country(request): 
+        #x={'people': People.objects.all()}
+        #return render(request , 'country.html' , x)   
 
 
 def country_id(request, id):
