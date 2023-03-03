07d3008e56b58004b35904cbbfb95d2b6d7bbe43
mero313 <ameer_mazin2001@yahoo.com>
2023-03-03 22:08:58 +0300

    desk

--- country/urls.py
+++ country/urls.py
@@ -1,10 +1,10 @@
-from . import views
 from django.urls import path , include
 from . import views 
 
 urlpatterns = [
      path('' , views.country1 , name='country1' ),
     path('<int:id>' , views.country_id , name='country_id'),
+    path('test/' , views.country , name='country' ),
     
     
    
--- country/views.py
+++ country/views.py
@@ -1,22 +1,20 @@
 from django.shortcuts import render
-from .models import People , Custom , Event , Food
+from .models import People, Custom, Event, Food
 
 # Create your views here.
 
 
+def country1(request):
+    x = {'people': People.objects.all()}
+    return render(request, 'country1.html', x)
 
-def country1(request): 
-    return render(request , 'country1.html' )   
 
-
-    #def country(request): 
-        #x={'people': People.objects.all()}
-        #return render(request , 'country.html' , x)   
+def country(request):
+     y={'people': People.objects.get(name='ameer')}
+     return render(request , 'country.html' , y)
 
 
 def country_id(request, id):
     x = {'event': Event.objects.get(people_id=People.objects.get(id=id)),
-        'people': People.objects.get(id=id),}
-    return render(request , 'country_id.html' , x  )
-
-
+         'people': People.objects.get(id=id), }
+    return render(request, 'country_id.html', x)
