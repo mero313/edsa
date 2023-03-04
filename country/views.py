8191da7930074bda941600632005c6340de81631
mero313 <ameer_mazin2001@yahoo.com>
2023-03-04 18:02:24 +0300

    laptop

--- country/views.py
+++ country/views.py
@@ -10,8 +10,8 @@ def country1(request):
 
 
 def country(request):
-     y={'people': People.objects.get(name='ameer')}
-     return render(request , 'country.html' , y)
+      y={'people': People.objects.get(name='ameer')}
+      return render(request , 'country.html' , y)
 
 
 def country_id(request, id):
