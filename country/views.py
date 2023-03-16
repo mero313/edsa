c22bf1330ea32d06127e96856ea5ccfb6e18e676
mero313 <ameer_mazin2001@yahoo.com>
2023-03-17 00:41:56 +0300

    desk

--- country/views.py
+++ country/views.py
@@ -13,13 +13,22 @@ def country1(request):
     x = {'event': Event.objects.get(people_id=People.objects.get(id=2)),
          'custom': Custom.objects.get(id=2),
          'people': People.objects.get(id=2),
-         'food':Food.objects.get(id=2),}
+         'food': Food.objects.get(id=2), }
     return render(request, 'country1.html', x)
 
 
 def index(request, id):
-    y = {'event': Event.objects.get(people_id=People.objects.get(id=id)),
-         'custom': Custom.objects.get(id=id),
-         'people': People.objects.get(id=id),
-          'food': Food.objects.get(id=id) ,}
+    if id == 4:
+        y = {'event': Event.objects.get(people_id=People.objects.get(id=id)),
+             'custom': Custom.objects.get(id=5),
+             'people': People.objects.get(id=id),
+             'food': Food.objects.get(id=id), }
+        
+    else:
+        y = {'event': Event.objects.get(people_id=People.objects.get(id=id)),
+             'custom': Custom.objects.get(id=id),
+             'people': People.objects.get(id=id),
+             'food': Food.objects.get(id=id), }
+
+
     return render(request, 'index.html', y)
